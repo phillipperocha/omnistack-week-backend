9cac7841ec03ec48fab5c658b913941bcbc10373
Phillippe Rocha <phillipperocha@gmail.com>
2019-08-06 22:30:18 -0300

    Receiving GET parameters from request and returning a JSON response. Auto server restart on changes.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
-// Importaremos o expressJS com o require()
 const express = require('express');
-
-// O express é uma função, que quando chamada cria um servidor
 const server = express();
 
-// Precisamos mapear uma rota e dizer o que fazer com a requisição e a resposta
-// Podiamos fazer GET, POST, PUT ou DELETE, mas vamos usar o get
 server.get('/', (req, res) => {
-    // Vamos retornar na response apenas um Hello World!
-    return res.send('Hello World!');
+    // Podemos pegar os parâmetros da requisição através da mesma pela query
+    let nome = req.query.nome;
+    let idade = req.query.idade;
+
+    // Como iremos trabalhar com API retornaremos JSON por isso
+    return res.json({"nome": nome, "idade": idade});
 });
 
-// Precisamos setar em que porta esse servidor vai ouvir, no caso 3333
 server.listen(3333);
