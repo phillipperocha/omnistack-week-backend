f8698917edc8b007db008c222491c779197ab9bc
Phillippe Rocha <phillipperocha@gmail.com>
2019-08-06 23:23:04 -0300

    Telling ExpressJS to use JSON and dealing with POST requests.

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,6 @@
-// Existem várias maneiras de chamar o server aqui dentro mas faremos o seguinte
-// Importaremos o express
 const express = require('express');
-// E usar o método do express específico pra rotas
 const routes = express.Router();
 
-// E agora atualizaremos o nosso routes no lugar do server
 routes.get('/', (req, res) => {
     let nome = req.query.nome;
     let idade = req.query.idade;
@@ -12,5 +8,9 @@ routes.get('/', (req, res) => {
     return res.json({"nome": nome, "idade": idade});
 });
 
-// E exportaremos as nossas rotas para que o server possa utilizá-las
+routes.post('/devs', (req, res) => {
+    // Agora sabemos ler o req.body e o devolveremos também como resposta
+    return res.json(req.body);
+})
+
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// Sempre que quisermos no node importar um recurso que criamos, que não é um module
-// Temos que passar o caminho do arquivo
 const routes = require('./routes')
 
 const server = express();
 
-// E agora vamos dizer ao nosso servidor para usar essas rotas
+// Vamos avisar ao Express que nas requisições as informações estarão em JSON
+server.use(express.json());
+
 server.use(routes);
 
 server.listen(3333);
